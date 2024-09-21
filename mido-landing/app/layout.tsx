3581e526560178605102736fa3f83fee9e23da68
Poulav Bhowmick <bpoulav@gmail.com>
2024-09-21 17:14:59 +0530

    added staking page

--- mido-landing/app/layout.tsx
+++ mido-landing/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import WalletContextProvider from "@/context/WalletContextProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <WalletContextProvider>
+
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </WalletContextProvider>
     </html>
   );
 }
